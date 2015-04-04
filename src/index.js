f7f82f46070f9ff03b317f54c35b3a780102a170
Nate Hunzaker <nate.hunzaker@gmail.com>
2015-04-04 13:33:29 -0400

    [update] Readme

--- src/index.js
+++ src/index.js
@@ -2,62 +2,62 @@
  * Foliage
  */
 
-import sprout from 'sprout-data'
+let sproute = require('sproute-data')
 
-export default class Foliage {
+module.exports = function Foliage(value, parent) {
+  this._value  = value
+  this._parent = parent
+}
 
-  constructor(value, parent) {
-    this._value  = value
-    this._parent = parent
-  }
+Foliage.prototype = {
 
   get(key, fallback) {
     return sprout.get(this._value, key, fallback)
-  }
+  },
 
   set(key, value) {
     this._value = sprout.assoc(this._value, key, value)
-  }
+  },
 
   branch(key, fallback) {
     return new Foliage(this.get(key, fallback), this)
-  }
+  },
 
   trunk() {
     return this._parent ? this._parent.trunk() : this
-  }
+  },
 
   keys() {
     return Object.keys(this._value)
-  }
+  },
 
   values() {
     return this.keys().map(key => this.get(key), this)
-  }
+  },
 
   map(fn, scope) {
     return this.values().map(fn, scope)
-  }
+  },
 
   filter(fn) {
     return this.values().filter(fn)
-  }
+  },
 
   find(fn) {
     return this.filter(fn)[0]
-  }
+  },
 
   reduce(fn, initial, scope) {
     return this.values().reduce(fn, initial, scope)
-  }
+  },
 
   value() {
     return this.valueOf()
-  }
+  },
 
   valueOf() {
     return this._value
-  }
+  },
 
   is(branch) {
     return this.valueOf() === branch.valueOf()
