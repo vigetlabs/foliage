f5825797c7a52dbf237bd8f7a7a6efbbe8dea86d
Nate Hunzaker <nate.hunzaker@gmail.com>
2015-05-04 07:43:48 -0400

    [update] Move fallback logic to get.js

--- src/get.js
+++ src/get.js
@@ -4,10 +4,13 @@
  *
  * @param {Object} obj  - The target object
  * @param {Array} keys - A list of string keys
+ * @param {any} fallback - If the value is undefined, a fallback
  */
 
 module.exports = function (obj, keys, fallback) {
-  return keys.reduce(function(tree, key) {
-    return tree ? tree[key] : fallback
+  let value = keys.reduce(function(tree, key) {
+    return tree ? tree[key] : undefined
   }, obj)
+
+  return value === undefined ? fallback : value
 }
